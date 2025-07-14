086982ea6fe842ca9baad8b398bcb9f3c9fdc473
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-07-14 14:53:24 +0530

    updated

--- RestaurantApp/obj/Debug/net9.0/RestaurantApp.AssemblyInfo.cs
+++ RestaurantApp/obj/Debug/net9.0/RestaurantApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d697a4e2318c36223347b357ec9127ae38a8f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
